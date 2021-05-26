150a2cb13bec31b317582686447f64460b034e02
Andres Efrain Chango Macas <achango@todo1.com>
2021-05-25 20:15:49 -0500

    init project

---
+++ stockList/stockList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  stockList
+//
+//  Created by Andres Efrain Chango Macas on 5/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ stockList/stockList/stockListApp.swift
@@ -0,0 +1,17 @@
+//
+//  stockListApp.swift
+//  stockList
+//
+//  Created by Andres Efrain Chango Macas on 5/25/21.
+//
+
+import SwiftUI
+
+@main
+struct stockListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
